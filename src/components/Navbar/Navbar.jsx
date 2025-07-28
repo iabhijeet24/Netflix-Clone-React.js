bf79f321bcb39958d86df710c8de91646485adb4
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-07-28 22:08:32 +0530

    Updated Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,8 @@ import './Navbar.css'
 import logo from '../../assets/logo.png'
 import searchIcon from '../../assets/search_icon.svg';
 import bellIcon from '../../assets/bell_icon.svg';
+import caretIcon from '../../assets/caret_icon.svg';
+import profileImage from '../../assets/profile_img.png';
 
 
 
@@ -12,7 +14,7 @@ const Navbar = () => {
   return (
     <div className='Navbar'>
       <div className="navbar-left">
-        <img src={logo} alt="logoImage" />
+        <img src={logo} alt="logoImage" className='logoImage'/>
 
         <ul>
           <li>Home</li>
@@ -24,8 +26,13 @@ const Navbar = () => {
       </div>
 
       <div className="navbar-right">
-        <img src={searchIcon} alt="searchIcon" />
-        <img src={bellIcon} alt="bellIcon" />
+        <img src={searchIcon} alt="searchIcon"  className='icons'/>
+        <img src={bellIcon} alt="bellIcon" className='icons'/>
+      </div>
+
+      <div className="navbar-profile">
+        <img src={profileImage} className='profile-image' />
+        <img src={caretIcon} className='caret-icon'/>
       </div>
     </div>
   )
