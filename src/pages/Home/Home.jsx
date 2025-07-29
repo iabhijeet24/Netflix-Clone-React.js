98d9a5d6fa295233e5ba3daaa630c222d741cac1
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-07-30 00:29:38 +0530

    updated hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
 import heroImage from '../../assets/hero_banner.jpg'
 import heroTitle from '../../assets/hero_title.png'
+import playIcon from '../../assets/play_icon.png'
+import infoIcon from '../../assets/info_icon.png'
 
 const Home = () => {
   return (
@@ -14,6 +16,12 @@ const Home = () => {
         <div className="hero-caption">
           <img src={heroTitle} className='caption-image'></img>
           <p className='caption-text'>Discovering his ties to an ancient secret order, a ypung man living in moder instanbul embarks a quest to save the city from a immortal enemy.</p>
+          
+          <div className="hero-button">
+             <button className='btn1'> <img src={playIcon} alt="icon" /> Play </button>
+             <button className='btn2'> <img src={infoIcon} alt="icon" /> More Info</button>
+          </div>
+         
         </div>
 
       </div>
