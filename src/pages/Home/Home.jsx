ec407e0f90d0d34fb7d0464ada39bb81a0b52387
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-07-30 00:00:55 +0530

    Added homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import heroImage from '../../assets/hero_banner.jpg'
+import heroTitle from '../../assets/hero_title.png'
 
 const Home = () => {
   return (
     <div className='HomePage'>
       <Navbar />
+
+      <div className="hero-page">
+        <img src={heroImage} className='hero-image' alt="Hero Banner" />
+        <div className="hero-caption">
+          <img src={heroTitle} className='caption-image'></img>
+          <p className='caption-text'>Discovering his ties to an ancient secret order, a ypung man living in moder instanbul embarks a quest to save the city from a immortal enemy.</p>
+        </div>
+
+      </div>
     </div>
   )
 }
